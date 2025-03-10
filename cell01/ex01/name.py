e569454ce42f24a875fcd967c0b30eab93112739
Chongtha <nowza285@gmail.com>
2025-03-10 13:36:43 +0700

    Add files via upload

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,3 +1,4 @@
-first_name = ("Chakkapong")
-last_name = ("Hongthai")
-print(first_name, last_name)
+first_name = "Chakkapong"
+last_name = "Hongthai"
+whole_name = first_name + " " + last_name
+print(whole_name)
