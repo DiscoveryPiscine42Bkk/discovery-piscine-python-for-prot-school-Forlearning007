15b07811096db874108b506433124c6029378c11
Chongtha <nowza285@gmail.com>
2025-03-11 10:19:27 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,13 @@
+Mult = int(input("Enter your first Number = "))
+Mult2 = int(input("Enter your second Number = "))
 
+result = Mult * Mult2
+
+print(f"{Mult} × {Mult2} = {result}")
+
+if result > 0:
+    print("The result is positive")
+elif result < 0:
+    print("The result is negative")
+else
+    print("The result is both positive and negative")
