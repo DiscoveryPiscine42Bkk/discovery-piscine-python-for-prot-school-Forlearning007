7020c8434161ac3efbb6b6713c01ed760a72ff1c
Chongtha <nowza285@gmail.com>
2025-03-11 15:01:43 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+Num = int(input("Give me the first number: "))
+Num1 = int(input("Give me the second number: "))
+print("Thank you!")
+Buak = Num + Num1
+print(str(Num) + " + " + str(Num1) + " = " + str(Buak))
+Loap = Num - Num1
+print(str(Num) + " - " + str(Num1) + " = " + str(Loap))
+Koon = Num * Num1
+print(str(Num) + " * " + str(Num1) + " = " + str(Koon))
+Han = Num // Num1
+print(str(Num) + " / " + str(Num1) + " = " + str(Han))
