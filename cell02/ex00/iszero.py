0cf8f2c2ae8b2d577c1eecf0c1b8825a0a9791d7
Chongtha <nowza285@gmail.com>
2025-03-11 09:02:11 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+Number = input('Enter your Number =')
+Number = int(Number)
+if Number == 0:
+    print('This number is equal to zero.')
+elif Number > 0:
+    print('This number is different from zero.')
