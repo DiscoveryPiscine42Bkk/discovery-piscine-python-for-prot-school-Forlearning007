6526f4eba7b8e1df106e56593b8d50e6c0269b5b
Chongtha <nowza285@gmail.com>
2025-03-11 11:03:45 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,1 +1,8 @@
-
+Hello = input('What you gotta say? =')
+while True:
+    Loop = input("Hello, What do you want?: ")
+    if Loop == "STOP":
+        print("OK")
+        break
+    else:
+        print("Understood.")
