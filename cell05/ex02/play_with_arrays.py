a83fff30e36519847af82d21d14e53a7c8072717
Chongtha <nowza285@gmail.com>
2025-03-12 09:57:54 +0700

    Create play_with_arrays.py

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,8 @@
+First_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+First_array = [x for x in First_array if x >= 5]
+
+Second_array = [x + 2 for x in First_array]
+
+print("First array:", First_array)
+print("Second array:", Second_array)
